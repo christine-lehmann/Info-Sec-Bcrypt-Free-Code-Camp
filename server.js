09ce416f1683b483619f89c11a101c359a4099f5
Christine Cion <cion.c.bscs@gmail.com>
2023-09-11 16:49:59 +0800

    hash and compare password

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.get('/',(req,res)=> {
 });
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    /*Store hash in your db*/
     console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare('passw0rd!', hash, (err, res) => {
-      console.log(res); //true
-    });
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        /*res == true or false*/
+        console.log(res);
+      });
   });
 
 
